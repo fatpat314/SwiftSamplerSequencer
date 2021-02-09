a8a7778e52389fe5c30d6a1e655793b0d9e96ace
Patrick <w.patrick.kelly@gmail.com>
2021-02-08 17:02:09 -0800

    Initial Commit

---
+++ Seqencer/SeqencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeqencerApp.swift
+//  Seqencer
+//
+//  Created by patrick kelly on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SeqencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
